7c69620d4cd65dd735432a5835c427469970d43d
JonatanASilva <jonatan.silva26@hotmail.com>
2022-03-21 21:20:39 -0300

    UC8 A1

---
+++ par.js
@@ -0,0 +1,9 @@
+const quantidadedealunos = 14;
+for (let numero = 0; numero <= 14; numero ++)
+if (numero == 0){
+    console.log("numero zero");
+}else if (numero %2 ==0){
+    console.log(" o numero é par " + numero);
+}else{
+    console.log(" o numero é impar " + numero);
+}
